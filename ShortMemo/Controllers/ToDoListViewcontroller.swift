3bf4e15f52c2c3f05e75c1641b96096e7af272a6
IlashaDogoni <fistonfire@mail.ru>
2022-03-20 15:29:36 +0600

    Create Item model and resolve Cell Reuse problem

--- ShortMemo/Controllers/ToDoListViewcontroller.swift
+++ ShortMemo/Controllers/ToDoListViewcontroller.swift
@@ -9,20 +9,35 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-        var itemArray = ["Buy aplles", "Buy milk", "Buy eggs"]
+        var itemArray = [Item]()
     let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
-            itemArray = items
+        
+        let newItem1 = Item()
+        newItem1.title = "Buy this"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy that"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Buy these"
+        itemArray.append(newItem3)
+        
+//        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
+ //            itemArray = items
         }
-    }
+   
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField()
         let alert = UIAlertController(title: "add new item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { action in
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
@@ -41,19 +56,16 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        tableView.deselectRow(at: indexPath, animated: true)
-       }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
 }
 
  
-
+}
---
+++ ShortMemo/DataModel/Item.swift
@@ -0,0 +1,13 @@
+//
+//  Item.swift
+//  ShortMemo
+//
+//  Created by Ilya Kokorin on 20.03.2022.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
